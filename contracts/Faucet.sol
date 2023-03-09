15b4cd463a38e7ff2c034906ece56e27a1d1ac9e
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-03-09 17:42:04 +0000

    Faucet allow anyone to withdraw an amount lower than .1ETH

---
+++ contracts/Faucet.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+contract Faucet {
+  function withdraw(uint _amount) public {
+    require(_amount <= 100000000000000000);
+    payable(msg.sender).transfer(_amount);
+  }
+
+  receive()external payable{}
+}
